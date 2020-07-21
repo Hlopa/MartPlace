44d10803eab8f1e05cd03db71841bb6f93d09322
Hlopa <bariolga@yandex.ru>
2020-07-21 14:21:14 +0300

    add new weekly slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,25 @@
 $(function () {
 
-    $('.slider__products-main').slick({
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "16px",
+        readOnly: true,
+        normalFill: "#dce1e7",
+        ratedFill: "#ffc000"
+      });
 
+$('.weekly-slider__box').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1
     }
     );
 
-    var mixer = mixitup('.newest__products');
+
 })
+
+
+/*
+
+var mixer = mixitup('.newest__products'); */
+
+
