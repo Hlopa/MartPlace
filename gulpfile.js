b6ea3de2f3f94dbc7a5f0ef3f1f333bb7d9ab1e1
Hlopa <bariolga@yandex.ru>
2020-07-27 23:27:15 +0300

    add rateyo

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task ("style", function(){
     return gulp.src ([
         "node_modules/normalize.css/normalize.css",
         "node_modules/magnific-popup/dist/magnific-popup.css",
-        "node_modules/rateyo/dist/jquery.rateyo.css",
+        "app/rateyo/dist/jquery.rateyo.css",
         "node_modules/slick-carousel/slick/slick.css"
     ])
     .pipe(concat("libs.min.css"))
@@ -38,7 +38,7 @@ gulp.task ("script", function(){
         "node_modules/slick-carousel/slick/slick.js",
         "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
         "node_modules/mixitup/dist/mixitup.js",
-        "node_modules/rateyo/dist/jquery.rateyo.js"
+        "app/rateyo/dist/jquery.rateyo.js"
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
